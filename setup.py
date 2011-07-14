3e543abed074ca28bcea47098ee439e4eed06b18
Erdem Agaoglu <erdem.agaoglu@gmail.com>
2011-07-14 09:54:06 +0300

    Added more setup info

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 name = "pyjasperclient"
-version = "0.1.0"
+version = "0.1.1"
 
 setup(
     name=name,
     version=version,
+    description='JasperServer SOAP client for Python',
     packages=['pyjasperclient',],
     install_requires = ['suds','elementtree'],
     url='https://github.com/agaoglu/pyjasperclient',
+    license='Apache',
     author='Erdem Agaoglu',
     author_email='erdem[dot]agaoglu[at]gmail.com'
 )
