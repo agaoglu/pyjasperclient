4d4bcba8258dd6f8501250d67da2bb9ebf9c5bbf
Erdem Agaoglu <erdem@nokta.com>
2014-09-20 10:09:32 +0300

    Removed elementtree dependency

--- pyjasperclient/jasperclient.py
+++ pyjasperclient/jasperclient.py
@@ -11,10 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-try:
-    from xml.etree import ElementTree as ET
-except ImportError, e:
-    from elementtree import ElementTree as ET
+from xml.etree import ElementTree as ET
 from suds.client import Client
 from suds.transport.http import HttpAuthenticated
 from operator import itemgetter
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 name = "pyjasperclient"
-version = "0.1.1"
+version = "0.2.2"
 
 setup(
     name=name,
     version=version,
     description='JasperServer SOAP client for Python',
     packages=['pyjasperclient',],
-    install_requires = ['suds','elementtree'],
+    install_requires = ['python>=2.5', 'suds'],
     url='https://github.com/agaoglu/pyjasperclient',
     license='Apache',
     author='Erdem Agaoglu',
